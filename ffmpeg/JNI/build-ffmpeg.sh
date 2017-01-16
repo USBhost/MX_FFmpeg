6d7d27fb5119b9f759c869cbadb12a3272489cbe
USBhost <josiahspore@gmail.com>
2017-01-15 16:23:40 -0800

    make build one shell and add cpu core checker

--- ffmpeg/JNI/build-ffmpeg.sh
+++ ffmpeg/JNI/build-ffmpeg.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+#set -x
 
-CPU_CORE=12
+CPU_CORE="$(cat /proc/cpuinfo | grep -c processor)"
 
 cd ffmpeg
 
 make distclean
-../config-ffmpeg.sh $1
+. ../config-ffmpeg.sh $1
 make clean
 make -j$CPU_CORE
 cd ..
 
-./build.sh ffmpeg.mx build $1
-./copy_to_$1.sh ffmpeg.mx
+. build.sh ffmpeg.mx build $1
+. copy_to_$1.sh ffmpeg.mx
 
 
